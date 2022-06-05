c18f222fa7cd0d36f370e3091f001386b13a6887
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-05 20:14:30 +0300

    not fixed

--- solution/index.js
+++ solution/index.js
@@ -1,11 +1,21 @@
-let myDiv = document.querySelector(".myFormList");
-let myDolists = document.querySelector("label");
+let myGeneralDiv = document.querySelector(".my-list");
+
 let myCounter = document.querySelector("span");
 
 
-myDiv.addEventListener("click", (e) => {
-    if(e.target.classList.contains("inputum"))
-        myCounter.innerText++
-        
-    
+myGeneralDiv.addEventListener("click", (e) => {
+    let myPar = e.target.nextElementSibling;
+    // console.log(myPar);
+    if(e.target.className == "inputum"){
+        if (true) {
+            e.target.nextElementSibling.innerHTML = `<del>${myPar.innerText}</del>`
+            e.target.nextElementSibling.style.color = "red"
+            myCounter.innerText++
+            
+        }else{
+            e.target.nextElementSibling.innerHTML = `<del>${myPar.innerText}</del>`
+            myCounter.innerText--
+        }
+            
+    }    
 }) 
