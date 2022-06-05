69671dbaf10d474515eb70bb174652b694a5eb45
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-05 21:46:29 +0300

    final fixes

--- solution/index.js
+++ solution/index.js
@@ -2,19 +2,16 @@ let myGeneralDiv = document.querySelector(".my-list");
 
 let myCounter = document.querySelector("span");
 
-
 myGeneralDiv.addEventListener("click", (e) => {
     let myPar = e.target.nextElementSibling;
-    // console.log(myPar);
+
     if(e.target.className == "inputum"){
         if (e.target.nextElementSibling.innerHTML != `<del>${myPar.innerText}</del>`) {
             e.target.nextElementSibling.innerHTML = `<del>${myPar.innerText}</del>`
             myCounter.innerText++
-            console.log(e.target.nextElementSibling.innerHTML);
         }else{
             e.target.nextElementSibling.innerHTML = `${myPar.innerText}`
             myCounter.innerText--
         }
-            
     }    
 }) 
