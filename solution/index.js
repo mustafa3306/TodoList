8f2f58d1c7349f1893caf8772cfaddb0be2db4ef
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-05 21:44:17 +0300

    finished

--- solution/index.js
+++ solution/index.js
@@ -7,13 +7,12 @@ myGeneralDiv.addEventListener("click", (e) => {
     let myPar = e.target.nextElementSibling;
     // console.log(myPar);
     if(e.target.className == "inputum"){
-        if (true) {
+        if (e.target.nextElementSibling.innerHTML != `<del>${myPar.innerText}</del>`) {
             e.target.nextElementSibling.innerHTML = `<del>${myPar.innerText}</del>`
-            e.target.nextElementSibling.style.color = "red"
             myCounter.innerText++
-            
+            console.log(e.target.nextElementSibling.innerHTML);
         }else{
-            e.target.nextElementSibling.innerHTML = `<del>${myPar.innerText}</del>`
+            e.target.nextElementSibling.innerHTML = `${myPar.innerText}`
             myCounter.innerText--
         }
             
